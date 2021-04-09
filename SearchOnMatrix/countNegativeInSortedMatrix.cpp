262a093e9947d3edea027ba1cb42ca4848700339
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-04-09 23:34:26 +0600

    Update countNegativeInSortedMatrix.cpp

--- SearchOnMatrix/countNegativeInSortedMatrix.cpp
+++ SearchOnMatrix/countNegativeInSortedMatrix.cpp
@@ -1,4 +1,7 @@
 #time: O(n+m)
+// find the first negative index in every row
+// suppose we have [5, 4, -1, -2, -3] ->we will start from rightmost corner and check if we  meet with negative, if we meet negative at first, it is gueranteed that right
+//after this first index all element will be negative since it's sorted.Here the first negative index is 2, so the negative count will be col - first negative index(5-2=3).
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
@@ -36,3 +39,45 @@ int main() {
 
     return 0;
 }
+//O(n + log(m))
+// in this optimized approach we will just the first index using binary search.
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int firstIndex(vector<int> &arr){
+        int start = 0;
+        int end = arr.size()-1;
+        int location = -1;
+        while (start <= end){
+            int mid = start + (end - start)/2;
+            if (arr[mid] < 0){
+                end = mid - 1;
+                location = mid;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        return location;
+    }
+    int countNegatives(vector<vector<int>>& grid) {
+        int row = grid.size();
+        int col = grid[0].size();
+        int i = 0;
+        int count = 0;
+        while (i < row){
+
+            int first = firstIndex(grid[i]);
+            if (first >= 0){
+                count += (col - first);
+                i ++;
+            }
+            else {
+                i++;
+            }
+
+        }
+        return count;
+    }
+};
