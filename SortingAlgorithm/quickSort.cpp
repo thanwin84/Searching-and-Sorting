1319fc8d3a6d97a529a1e2d9fa91b021061505bb
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-04-23 19:24:17 +0600

    Update quickSort.cpp

--- SortingAlgorithm/quickSort.cpp
+++ SortingAlgorithm/quickSort.cpp
@@ -37,3 +37,49 @@ public:
         return pindex;
     }
 };
+//improve quicksort using randomized
+class Solution
+{
+public:
+    //Function to sort an array using quick sort algorithm.
+    void quickSort(int arr[], int low, int high)
+    {
+        // code here
+        if (low < high){
+            int pindex= partition(arr, low, high);
+            quickSort(arr, low, pindex -1);
+            quickSort(arr, pindex + 1, high);
+        }
+    }
+
+public:
+
+    int partition (int arr[], int low, int high)
+    {
+        // Your code here
+        srand(time(nullptr));
+        int random = low + rand() % (high - low);
+        int temp = arr[random];
+        arr[random] = arr[high];
+        arr[high] = temp;
+        int pindex = partition2(arr, low, high);
+        return pindex;
+
+    }
+    int partition2(int arr[], int low, int high){
+        int pindex = 0;
+        int pivot = arr[high];
+        for (int i = 0; i < high; i++){
+            if (arr[i] <= pivot){
+                int temp = arr[i];
+                arr[i] = arr[pindex];
+                arr[pindex] = temp;
+                pindex++;
+            }
+        }
+        int temp = arr[pindex];
+        arr[pindex] = arr[high];
+        arr[high] = temp;
+        return pindex;
+    }
+};
