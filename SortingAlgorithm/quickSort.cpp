2883eacbabe199a6be2b3d73e9e81d58873ba782
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-04-24 10:00:48 +0600

    Update quickSort.cpp

--- SortingAlgorithm/quickSort.cpp
+++ SortingAlgorithm/quickSort.cpp
@@ -38,7 +38,8 @@ public:
     }
 };
 ## Randomized quicksort
-## Since the partition procedure has linear time complexity, the overall time complexity, in this case, is quadratic. This is the worst-case scenario for our input array.
+// suppose we  all have  repeating character in array [4, 4, 4, 4, 4, 4]. if we use single partition method, in the first partition we will get 2 groups, one is empty and
+//another is n-1, the next will be n-2, n-3  and so on which makes O(n^2). We can can handle duplicate keys by using three ways partitioning method such as Dutch national flag
 class Solution
 {
 public:
@@ -73,5 +74,53 @@ public:
         arr[pindex] = arr[high];
         arr[high] = temp;
         return pindex;
+    }
+};
+//improved quicksort to handle repeating keys
+class Solution
+{
+public:
+    //Function to sort an array using quick sort algorithm.
+    void quickSort(int arr[], int low, int high)
+    {
+        // code here
+        if (low < high){
+            pair<int, int> p = dNL_partition(arr, low, high);
+            quickSort(arr, low, p.first-1);
+            quickSort(arr, p.second + 1, high);
+        }
+    }
+
+public:
+
+    pair<int, int> dNL_partition(int arr[], int low, int high){
+        srand(time(nullptr));
+        int random = low + rand() % (high - low);
+        int pivot = arr[high];
+        int firstPIndex = low;
+        //forward pass to check if arr[i] is less than the pivot
+        for(int i = low; i < high; i++){
+            if(arr[i] < pivot){
+                int temp = arr[i];
+                arr[i] = arr[firstPIndex];
+                arr[firstPIndex] = temp;
+                firstPIndex += 1;
+            }
+        }
+        int secondPINdex = high;
+        //backward pass to check if arr[i] is greater than pivot
+        // if we get arr[i] < pivot, it means we are in the lesser region. In this case, we break the loop
+        for(int i = high; i >= low; i--){
+            if (arr[i] > pivot){
+                int temp = arr[i];
+                arr[i] = arr[secondPINdex];
+                arr[secondPINdex] = temp;
+                secondPINdex--;
+            }
+            else if (arr[i] < pivot){
+                break;
+            }
+        }
+        return {firstPIndex, secondPINdex};
     }
 };
